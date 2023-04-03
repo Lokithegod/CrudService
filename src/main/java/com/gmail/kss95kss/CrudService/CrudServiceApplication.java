d47506ed0fac7413533c6b39518dcd12f3330509
iuad1cyj <sergii.s.kryvenko@raiffeisen.ua>
2023-04-03 18:35:28 +0300

    first commit

---
+++ src/main/java/com/gmail/kss95kss/CrudService/CrudServiceApplication.java
@@ -0,0 +1,15 @@
+package com.gmail.kss95kss.CrudService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class CrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudServiceApplication.class, args);
+	}
+
+}
