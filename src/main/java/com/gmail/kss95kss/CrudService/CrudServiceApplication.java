4a990297f0c5e06199fdbf7520f42fb95a40399d
iuad1cyj <sergii.s.kryvenko@raiffeisen.ua>
2023-12-12 10:39:00 +0200

    grafana

--- src/main/java/com/gmail/kss95kss/CrudService/CrudServiceApplication.java
+++ src/main/java/com/gmail/kss95kss/CrudService/CrudServiceApplication.java
@@ -3,13 +3,22 @@ package com.gmail.kss95kss.CrudService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class CrudServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudServiceApplication.class, args);
+    }
+
+/*    @Bean
+    FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+        };
+    }*/
 
 }
